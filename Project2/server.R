99aab2774f497094dffcef444377e42462059605
Ania <anna.giczewska90@gmail.com>
2024-07-09 13:37:41 -0400

    commit with a first Shiny App, but not working as desired yet

--- Project2/server.R
+++ Project2/server.R
@@ -1,28 +1,69 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+# server.R
 library(shiny)
+library(ggplot2)
+library(ggcorrplot)
+library(dplyr)
+library(reshape2)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+shinyServer(function(input, output) {
+  data_filtered <- reactive({
+    if (input$family == "All") {
+      data
+    } else {
+      data %>% filter(family == input$family)
+    }
+  })
+  
+  output$barPlot <- renderPlot({
+    input$plot_button
+    isolate({
+      df <- data_filtered()
+      melted_data <- melt(df, id.vars = c("name", "id", "family", "order", "genus"))
+      nutrition_data <- melted_data[melted_data$variable %in% c("calories", "fat", "sugar", "carbohydrates", "protein"),]
+      
+      ggplot(nutrition_data, 
+             aes(x = name, y = value, fill = variable)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Nutritional Composition of Fruits", x = "Fruit", y = "Nutritional Value") +
+        theme_minimal() +
+        coord_flip() +
+        facet_wrap(~ family, scales = "free_x")
     })
-
-}
+  })
+  
+  output$boxPlot <- renderPlot({
+    input$plot_button
+    isolate({
+      df <- data_filtered()
+      melted_data <- melt(df, id.vars = c("name", "id", "family", "order", "genus"))
+      nutrition_data <- melted_data[melted_data$variable %in% c("calories", "fat", "sugar", "carbohydrates", "protein"),]
+      
+      ggplot(nutrition_data, 
+             aes(x = variable, y = value, fill = variable)) +
+        geom_boxplot() +
+        labs(title = "Distribution of Nutritional Values Across Fruits", x = "Nutritional Element", y = "Value") +
+        theme_minimal()
+    })
+  })
+  
+  output$corrPlot <- renderPlot({
+    input$plot_button
+    isolate({
+      df <- data_filtered()
+      if (nrow(df) > 1) {
+        numeric_data <- df %>% select(calories, fat, sugar, carbohydrates, protein)
+        corr_matrix <- cor(numeric_data, use = "complete.obs")
+        
+        ggcorrplot(corr_matrix, method = "circle", lab = TRUE)
+      } else {
+        output$warningText <- renderText({
+          "Not enough data to generate correlation plot for this family."
+        })
+      }
+    })
+  })
+  
+  output$warningText <- renderText({
+    ""
+  })
+})
--- Project2/ui.R
+++ Project2/ui.R
@@ -1,33 +1,32 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+# ui.R
 library(shiny)
+library(ggplot2)
+library(ggcorrplot)
 
-# Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+shinyUI(fluidPage(
+  titlePanel("Fruit Nutrition Analysis"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("family", "Select Family:", choices = c("All", unique(data$family))),
+      actionButton("plot_button", "Generate Plots")
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Description", 
+                 h4("Dataset Description"),
+                 p("This dataset contains nutritional information of various fruits...")),
+        tabPanel("Plots",
+                 conditionalPanel(
+                   condition = "input.plot_button > 0",
+                   plotOutput("barPlot"),
+                   plotOutput("boxPlot"),
+                   plotOutput("corrPlot"),
+                   textOutput("warningText")
+                 )
         )
+      )
     )
-)
+  )
+))
