b8a1fd3f09e9b32760bd273db88b2d6e2620bb31
Ania <anna.giczewska90@gmail.com>
2024-07-09 14:59:02 -0400

    first 2 tabs are finalized, 3rd tab needs to be improved

--- Project2/server.R
+++ Project2/server.R
@@ -5,16 +5,141 @@ library(ggcorrplot)
 library(dplyr)
 library(reshape2)
 library(DT)
+library(httr)
+library(jsonlite)
+
+# Define functions to query the Fruityvice API
+get_all_fruits <- function() {
+  url <- "https://www.fruityvice.com/api/fruit/all"
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_family_by_name <- function(family) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/family/", family)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_order_by_name <- function(order) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/order/", order)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_genus_by_name <- function(genus) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/genus/", genus)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_fruits_by_calories <- function(min_calories, max_calories) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/calories?min=", min_calories, "&max=", max_calories)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_fruits_by_fat <- function(min_fat, max_fat) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/fat?min=", min_fat, "&max=", max_fat)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_fruits_by_sugar <- function(min_sugar, max_sugar) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/sugar?min=", min_sugar, "&max=", max_sugar)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_fruits_by_carbohydrates <- function(min_carbohydrates, max_carbohydrates) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/carbohydrates?min=", min_carbohydrates, "&max=", max_carbohydrates)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
+
+get_fruits_by_protein <- function(min_protein, max_protein) {
+  url <- paste0("https://www.fruityvice.com/api/fruit/protein?min=", min_protein, "&max=", max_protein)
+  response <- httr::GET(url)
+  if (httr::status_code(response) == 200) {
+    data <- fromJSON(httr::content(response, "text", encoding = "UTF-8"))
+    return(data)
+  } else {
+    return(NULL)
+  }
+}
 
 shinyServer(function(input, output) {
   data <- reactiveVal()
   
-  observeEvent(input$download_button, {
-    response <- httr::GET(input$api_url)
-    if (status_code(response) == 200) {
-      data_df <- fromJSON(content(response, "text")) %>% 
-        unnest_wider(nutritions) %>%
-        filter(name != "Hazelnut")
+  observeEvent(input$query_button, {
+    query_type <- input$query_type
+    query_param <- input$query_param
+    data_df <- NULL
+    
+    if (query_type == "All Fruits") {
+      data_df <- get_all_fruits()
+    } else if (query_type == "Family by Name") {
+      data_df <- get_family_by_name(query_param)
+    } else if (query_type == "Order by Name") {
+      data_df <- get_order_by_name(query_param)
+    } else if (query_type == "Genus by Name") {
+      data_df <- get_genus_by_name(query_param)
+    } else if (query_type == "Calories Range") {
+      data_df <- get_fruits_by_calories(input$min_calories, input$max_calories)
+    } else if (query_type == "Fat Range") {
+      data_df <- get_fruits_by_fat(input$min_fat, input$max_fat)
+    } else if (query_type == "Sugar Range") {
+      data_df <- get_fruits_by_sugar(input$min_sugar, input$max_sugar)
+    } else if (query_type == "Carbohydrates Range") {
+      data_df <- get_fruits_by_carbohydrates(input$min_carbohydrates, input$max_carbohydrates)
+    } else if (query_type == "Protein Range") {
+      data_df <- get_fruits_by_protein(input$min_protein, input$max_protein)
+    }
+    
+    if (!is.null(data_df)) {
+      data_df <- data_df %>%
+        unnest_wider(nutritions) #%>%
+        #filter(name != "Hazelnut")
       data(data_df)
     } else {
       showModal(modalDialog(
--- Project2/ui.R
+++ Project2/ui.R
@@ -2,7 +2,7 @@
 library(shiny)
 library(ggplot2)
 library(ggcorrplot)
-library(DT) 
+library(DT)
 
 shinyUI(fluidPage(
   titlePanel("Fruit Nutrition Analysis"),
@@ -11,8 +11,38 @@ shinyUI(fluidPage(
     sidebarPanel(
       conditionalPanel(
         condition = "input.tabs == 'Data Download'",
-        textInput("api_url", "API URL:", value = "https://www.fruityvice.com/api/fruit/all"),
-        actionButton("download_button", "Download Data")
+        selectInput("query_type", "Select Query Type:", choices = c("All Fruits", "Family by Name", "Order by Name", "Genus by Name", "Calories Range", "Fat Range", "Sugar Range", "Carbohydrates Range", "Protein Range")),
+        conditionalPanel(
+          condition = "input.query_type != 'All Fruits' && input.query_type != 'Calories Range' && input.query_type != 'Fat Range' && input.query_type != 'Sugar Range' && input.query_type != 'Carbohydrates Range' && input.query_type != 'Protein Range'",
+          textInput("query_param", "Enter Query Parameter:", value = "")
+        ),
+        conditionalPanel(
+          condition = "input.query_type == 'Calories Range'",
+          numericInput("min_calories", "Min Calories:", value = 0),
+          numericInput("max_calories", "Max Calories:", value = 1000)
+        ),
+        conditionalPanel(
+          condition = "input.query_type == 'Fat Range'",
+          numericInput("min_fat", "Min Fat:", value = 0),
+          numericInput("max_fat", "Max Fat:", value = 1000)
+        ),
+        conditionalPanel(
+          condition = "input.query_type == 'Sugar Range'",
+          numericInput("min_sugar", "Min Sugar:", value = 0),
+          numericInput("max_sugar", "Max Sugar:", value = 1000)
+        ),
+        conditionalPanel(
+          condition = "input.query_type == 'Carbohydrates Range'",
+          numericInput("min_carbohydrates", "Min Carbohydrates:", value = 0),
+          numericInput("max_carbohydrates", "Max Carbohydrates:", value = 1000)
+        ),
+        conditionalPanel(
+          condition = "input.query_type == 'Protein Range'",
+          numericInput("min_protein", "Min Protein:", value = 0),
+          numericInput("max_protein", "Max Protein:", value = 1000)
+        ),
+        actionButton("query_button", "Query API"),
+        downloadButton("download_data", "Download Data")
       ),
       conditionalPanel(
         condition = "input.tabs == 'Data Exploration'",
@@ -35,12 +65,11 @@ shinyUI(fluidPage(
                    tags$li("Data Download: Allows the user to download and subset data."),
                    tags$li("Data Exploration: Allows the user to visualize and analyze the data.")
                  ),
-                 img(src = "fruit_image.jpg", height = "400px")
+                 img(src = "fruit_image.jpg", height = "300px")  # Ensure this path is correct
         ),
         
         tabPanel("Data Download", 
-                 DTOutput("data_table"),
-                 downloadButton("download_data", "Download Data")
+                 DTOutput("data_table")
         ),
         
         tabPanel("Data Exploration",
